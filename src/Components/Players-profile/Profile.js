702e93807023177f62d9a5fa515bedbbd93ebdb4
Rafi-Genon <mdabulhasannurrafi@gmail.com>
2021-03-01 23:25:17 +0600

    font awsome added

--- src/Components/Players-profile/Profile.js
+++ src/Components/Players-profile/Profile.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 import './profile.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
 const Profile = (props) => {
     const { name, salary, picture, run, wicket } = props.playersDetails;
     const playerSelected = props.playerSelected
@@ -15,7 +18,7 @@ const Profile = (props) => {
                         I am very good plaayer. i played in so many country. I love to play cricket. I will do well in your team
                     </Card.Text>
                     <p>My make {run} run & {wicket} wicket</p>
-                    <Button variant="primary" onClick={() => playerSelected(props.playersDetails)}>Add me</Button>
+                    <Button variant="primary" onClick={() => playerSelected(props.playersDetails)}><FontAwesomeIcon icon={faPlus} /> Add me</Button>
                 </Card.Body>
             </Card>
         </div>
